367ab06d354da185490ca5bd3524b6405ed933b9
fquaranta <fquaranta00@hotmail.com>
2023-05-16 21:17:01 -0300

    se agregó switch con 3 pacientes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /*
 
 Programa para gestion en salud, de pacientes y obras sociales
-En el mismo encontraran 1 funcion y 1 switch:
+En el mismo encontraran 1 funcion y 1 Switch:
                 1. Función de login
                 2. Switch para seleccionar paciente e informacion general
 */
@@ -35,14 +35,15 @@ function login(usuarioLower, contrasena) {
 let intentos = 3;
 let loginKey = false;
 
+//While para validar usuario & contraseña
 while (intentos > 0 && loginKey === false) {
     let usuario = prompt("Ingrese Usuario o Mail: ");
-    let usuarioLower = usuario.toLowerCase();
+    let usuarioLower = usuario.toLowerCase(); //acá normalizamos el texto
     let contrasena = prompt("Ingrese su contraseña: "); 
     loginKey = login(usuarioLower, contrasena); //Acá se ejecuta la funcion login
     console.log("Resultado de la función: " + loginKey);    
     if (loginKey === false) {
-        intentos--;
+        intentos--; //por cada intento fallido descuenta 1 hasta llegar a cero
         console.log("Intentos restantes: " + intentos);
     }
 }
@@ -50,164 +51,93 @@ while (intentos > 0 && loginKey === false) {
 if (intentos === 0) {
     console.log("Se agotaron los intentos. Programa finalizado.");
 } else {
-    let numeroPaciente = prompt(
+
+//Comienzo Switch donde se selecciona el/la paciente y se trae información registrada junto con un listado-check de las sesiones tomadas
+
+let numeroPaciente = prompt(
     "Seleccione el paciente:\n1. Juancito Perez\n2. Francisca Lemes\n3. Jacinto Ceres"
-    );
-  // Comienzo del switch
+);
+
+// Comienzo del switch
+switch (numeroPaciente) {
+    case "1":
+        // Información del paciente Juancito Perez
+        let nombrePaciente1 = "Juancito";
+        let apellidoPaciente1 = "Perez";
+        let obraSocialPaciente1 = "Obra Social A";
+        let sesionesTotalesPaciente1 = 10;
+        let sesionesEfectuadasPaciente1 = prompt("Indique sesiones efectuadas: ");
+
+        console.log("Nombre y apellido: " + nombrePaciente1 + " " + apellidoPaciente1);
+        console.log("Obra social: " + obraSocialPaciente1);
+        console.log("Sesiones totales: " + sesionesTotalesPaciente1);
+        console.log("Sesiones efectuadas: " + sesionesEfectuadasPaciente1);
+
+        alert("Nombre y apellido: " + nombrePaciente1 + " " + apellidoPaciente1);
+        alert("Obra social: " + obraSocialPaciente1);
+        alert("Sesiones totales: " + sesionesTotalesPaciente1);
+        alert("Sesiones efectuadas: " + sesionesEfectuadasPaciente1);
+
+        // For bastante inutil pero es el único que se me ocurrió je, perdón
+        for (let i = 1; i <= sesionesEfectuadasPaciente1; i++) {
+            console.log("Sesión " + i + ": Realizada");
+        }
+        break;
+
+    case "2":
+        // Información del paciente Francisca Lemes
+        let nombrePaciente2 = "Francisca";
+        let apellidoPaciente2 = "Lemes";
+        let obraSocialPaciente2 = "Obra Social B";
+        let sesionesTotalesPaciente2 = 15;
+        let sesionesEfectuadasPaciente2 = prompt("Indique sesiones efectuadas: ");;
+
+        console.log("Nombre y apellido: " + nombrePaciente2 + " " + apellidoPaciente2);
+        console.log("Obra social: " + obraSocialPaciente2);
+        console.log("Sesiones totales: " + sesionesTotalesPaciente2);
+        console.log("Sesiones efectuadas: " + sesionesEfectuadasPaciente2);
+
+        alert("Nombre y apellido: " + nombrePaciente2 + " " + apellidoPaciente2);
+        alert("Obra social: " + obraSocialPaciente2);
+        alert("Sesiones totales: " + sesionesTotalesPaciente2);
+        alert("Sesiones efectuadas: " + sesionesEfectuadasPaciente2);
+
+
+
+        //bucle for
+        for (let i = 1; i <= sesionesEfectuadasPaciente2; i++) {
+            console.log("Sesión " + i + ": Realizada");
+        }
+        break;
+
+    case "3":
+        // Información del paciente Jacinto Ceres
+        let nombrePaciente3 = "Jacinto";
+        let apellidoPaciente3 = "Ceres";
+        let obraSocialPaciente3 = "Obra Social C";
+        let sesionesTotalesPaciente3 = 8;
+        let sesionesEfectuadasPaciente3 = prompt("Indique sesiones efectuadas: ");;
+
+        console.log("Nombre y apellido: " + nombrePaciente3 + " " + apellidoPaciente3);
+        console.log("Obra social: " + obraSocialPaciente3);
+        console.log("Sesiones totales: " + sesionesTotalesPaciente3);
+        console.log("Sesiones efectuadas: " + sesionesEfectuadasPaciente3);
+
+        alert("Nombre y apellido: " + nombrePaciente3 + " " + apellidoPaciente3);
+        alert("Obra social: " + obraSocialPaciente3);
+        alert("Sesiones totales: " + sesionesTotalesPaciente3);
+        alert("Sesiones efectuadas: " + sesionesEfectuadasPaciente3);
+
+        //bucle for
+        for (let i = 1; i <= sesionesEfectuadasPaciente3; i++) {
+            console.log("Sesión " + i + ": Realizada");
+        }
+        break;
+
+    default:
+        console.log("Opción inválida");
+        break;
+}
 }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// Comienzo función de login
-// se envía usuario y contraseña y devuelve true or false
-// function login(usuarioLower, contrasena) {
-//     const usuarioValidador = "fernando";
-//     const mailValidador = "fquaranta00@gmail.com";
-//     const passValidador = "p3p3";
-//     if (
-//         (usuarioLower == usuarioValidador || usuarioLower == mailValidador) &&
-//         contrasena == passValidador
-//     ) {
-//         alert("Usuario correcto");
-//         console.log("Usuario correcto");
-//         alert("Bienvenido " + usuarioValidador);
-//         console.log("Bienvenido " + usuarioLower);
-//         return true;
-//     } else {
-//         alert("Usuario/contraseña invalida");
-//         console.log("Usuario/contraseña invalida");
-//         alert("Intente nuevamente");
-//         console.log("Intente nuevamente");
-//         return false;
-//         }
-// }
-
-// FIN función de login
-
-//Comienzo funcion paciente donde, ingresando el nombre, se recupera que obra social tiene y cuantas sesiones disponibles tiene
-// if(numeroPaciente == 1) {
-
-// FIN funcion paciente
-
-// let intentos = 3;
-// let loginKey = false;
-
-// do {
-//     for(let i=0; i<intentos; i++){  
-//         let usuario = prompt("Ingrese Usuario ó Mail: ");
-//         let usuarioLower = usuario.toLowerCase();
-//         let contrasena = prompt("Ingrese su password: ");
-        
-//         loginKey = login(usuarioLower, contrasena); //En este punto se ejecuta la funcion login
-//         console.log("resultado de la funcion: " + loginKey);
-        
-//         if(loginKey == true){
-//             break;
-//         }else {
-//             intentos--;
-//             console.log("Intentos restantes: " + intentos)
-//         }
-//         if(intentos == 0) {
-//             break
-//         }
-//     }
-// }while (loginKey == false);
-
-// let numeroPaciente = prompt(
-//   "Seleccione la/él paciente:\n1. Juancito Perez\n2. Francisca Lemes\n1. Jacinto Ceres"
-// );
-
-// var pacienteKey = paciente(numeroPaciente); //En este punto se ejecuta paciente
-// console.log("Paciente: " + numeroPaciente);
-
-// switch (numeroPaciente) {
-//   case "1":
-//     let pacienteNombre = "Juancito Perez";
-//     let pacienteOS = "OSDE";
-//     let sesionesDisponibles = 10;
-//     let sesionesTomadas = 0;
-
-//     for(let i = sesionesDisponibles; i > 0; i--){
-//         sesionesTomadas = sesionesTomadas++
-//         sesionesDisponibles = sesionesDisponibles--
-//         console.log(
-//             "El/la Paciente " +
-//               pacienteNombre +
-//               " tiene obra social " +
-//               pacienteOS +
-//               " y cuenta con " +
-//               sesionesDisponibles +
-//               " sesiones disponibles"
-//           );
-//     break
-//     }
-
-//     break;
-//   case "2":
-//     break;
-//   case "3":
-//     break;
-//   default:
-//     alert("No seleccionó ningún paciente valido")
-//     console.log("No seleccionó ningún paciente valido")
-//     break;
-// }
-
-// var vidas = 3;
-
-// const NUMEROMAGICO = 7;
-
-// var pedirNumero = 0;
-
-// for (let i = 0; i < 50; i++) {
-//   //modulo...%
-//   pedirNumero = parseInt(prompt("elegir numero"));
-
-//   if (pedirNumero == NUMEROMAGICO) {
-//     alert("GANASTE!!!");
-//     break;
-//   } else {
-//     alert("intente de nuevo");
-//     vidas--;
-//     console.log(vidas);
-//   }
-// }
-
-// var dinero = 15000
-
-// var opcion = prompt("ingrese alguna opcion del cajero")
-
-// switch(opcion) {
-//     case "1":
-//       alert("Extraer")
-//        var extraDinero = parseInt(prompt("ingrese un monto a extraer"))
-//        dinero = dinero - extraDinero
-//        alert("te quedan " + dinero)
-
-//       break
-//     case "2":
-//         alert("Consultar")
-//         alert("usted tiene el saldo: " + dinero)
-//       break
-//     case "3":
-//         alert("transferir")
-//     break
-//     default:
-//         alert("no ha elegido una opcion")
-//     break;
-//    }
